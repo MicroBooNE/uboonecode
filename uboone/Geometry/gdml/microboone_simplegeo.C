721ca258a746ae72d92f9c7cecb1bb9d740125d7
Adam Patch <patch@fnal.gov>
2010-10-29 18:54:56 +0000

    organizing gdml files

---
+++ uboone/Geometry/gdml/microboone_simplegeo.C
@@ -0,0 +1,28 @@
+microboone_geo(TString volName="")
+{
+  gSystem->Load("libGeom");
+  gSystem->Load("libGdml");
+
+  TGeoManager::Import("microboone.gdml");
+
+  TList* mat = gGeoManager->GetListOfMaterials();
+  TIter next(mat);
+  TObject *obj;
+  while (obj = next()) {
+    obj->Print();
+  }
+
+  gGeoManager->CheckOverlaps(0.01);
+  gGeoManager->PrintOverlaps();
+  gGeoManager->SetMaxVisNodes(70000);
+
+  //gGeoManager->GetTopVolume()->Draw();
+  //if ( ! volName.IsNull() ) gGeoManager->FindVolumeFast(volName)->Draw("ogl");
+  //gGeoManager->FindVolumeFast("volFieldCage")->Draw();
+
+  TFile *tf = new TFile("microboone.root", "RECREATE");
+ 
+  gGeoManager->Write();
+
+  tf->Close();
+}
