c124e053241073888e7d0a8825c3d6ecfc204d88
Adam Patch <patch@fnal.gov>
2011-02-24 17:35:57 +0000

    add GetWeight commands, computation of TPC LAr mass

--- uboone/Geometry/gdml/microboone_geo.C
+++ uboone/Geometry/gdml/microboone_geo.C
@@ -31,13 +31,33 @@ microboone_geo(TString volName="")
   }
 
   gGeoManager->GetTopNode();
-  gGeoManager->CheckOverlaps(0.01);
+  gGeoManager->CheckOverlaps(0.0000001);
   gGeoManager->PrintOverlaps();
   gGeoManager->SetMaxVisNodes(70000);
 
   //gGeoManager->GetTopVolume()->Draw();
   //if ( ! volName.IsNull() ) gGeoManager->FindVolumeFast(volName)->Draw("ogl");
-  gGeoManager->FindVolumeFast("volWorld")->Draw("ogl");
+  //gGeoManager->FindVolumeFast("volWorld")->Draw("ogl");
+  gGeoManager->FindVolumeFast("volTPC")->Draw("ogl");
+  //gGeoManager->FindVolumeFast("volTPCPlane")->Draw("ogl");
+
+  TGeoVolume *TPC = gGeoManager->FindVolumeFast("volTPC");
+  float m_tpc = TPC->Weight();
+  TGeoVolume *Cathode = gGeoManager->FindVolumeFast("volCathodePlate");
+  float m_cathode = Cathode->Weight();
+  TGeoVolume *Ground = gGeoManager->FindVolumeFast("volGroundPlate");
+  float m_ground = Ground->Weight();
+  TGeoVolume *UVPlane = gGeoManager->FindVolumeFast("volTPCPlane");
+  float m_uvplane = UVPlane->Weight();
+  TGeoVolume *YPlane = gGeoManager->FindVolumeFast("volTPCPlaneVert");
+  float m_yplane = YPlane->Weight();
+  TGeoVolume *FieldCageH = gGeoManager->FindVolumeFast("volFieldCageTubeTop");
+  float m_fchoriz = FieldCageH->Weight();
+  TGeoVolume *FieldCageV = gGeoManager->FindVolumeFast("volFieldCageTubeFront");
+  float m_fcvert = FieldCageV->Weight();
+
+  float m_tpc_argon = m_tpc - ( m_cathode + m_ground + 2*m_uvplane + m_yplane + 50*(m_fchoriz + m_fcvert));
+  cout << "LAr weight in TPC = " << m_tpc_argon << " kg\n" <<endl;
 
   TFile *tf = new TFile("microboone.root", "RECREATE");
  
