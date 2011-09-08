e3b1557766de89422203da3385979a71dbc50a5f
Brian Rebel <brebel@fnal.gov>
2011-09-08 21:44:45 +0000

    Make separate modules for producing calibrated wire objects for T962 and MicroBooNE. This was done by simply copying CalWire.* and cutting out the bits that were for the wrong experiment. I made the CalWireMicroBooNE file look like the version Herb had in revision 1741 except I removed the commented out lines. The caldata.fcl file now has complete entries for both the microboone and T962 caldata modules. To recover the old behavior (ie use the CalWire module code which is not yet removed) you need only change the module_type in either configuration to CalWire

---
+++ uboone/CalData/CalWireMicroBooNE_module.cc
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////////
+// $Id: CalWireMicroBooNE.cxx,v 1.36 2010/09/15  bpage Exp $
+//
+// CalWireMicroBooNE class
+//
+// brebel@fnal.gov
+//
+////////////////////////////////////////////////////////////////////////
+// Framework includes
+#include "art/Framework/Core/ModuleMacros.h" 
+
+#include "CalData/CalWireMicroBooNE.h"
+
+namespace caldata{
+
+  DEFINE_ART_MODULE(CalWireMicroBooNE);
+  
+} // end namespace caldata
+
