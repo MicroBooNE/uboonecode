68ca31a572147084d3b4e5d75f9268ace5ddeb0e
Brian Page <bpage@fnal.gov>
2011-11-09 22:05:53 +0000

    Oops, try again.

---
+++ uboone/CalData/MicrobooneResponseFile/MicrobooneResp_module.cc
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////////
+// $Id: MicrobooneResp.cxx,v 1.0 2010/09/15  bpage Exp $
+//
+// MicrobooneResp class
+//
+// pagebri3@msu.edu
+//
+////////////////////////////////////////////////////////////////////////
+// Framework includes
+#include "art/Framework/Core/ModuleMacros.h" 
+
+#include "MicrobooneResp.h"
+
+namespace uboone{
+
+  DEFINE_ART_MODULE(MicrobooneResp);
+  
+} // end namespace uboone
+
