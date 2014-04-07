c65386f51e5fbf1ec8085d887cd608ec6b3cc01c
Ariana Hackenburg <ahack379@uboonegpvm02.fnal.gov>
2014-04-07 15:03:46 -0500

    Added some Geometry information for study using LArTF

--- uboone/Geometry/gdml/microboone/GenerateMicroBooNEGDML.sh
+++ uboone/Geometry/gdml/microboone/GenerateMicroBooNEGDML.sh
@@ -8,6 +8,11 @@
 ./generate_gdml.pl -w 1 -i microboone-gdml-parameters.xml -o microboone-gdml-fragments.xml
 ./make_gdml.pl -i microboone-gdml-fragments.xml -o microboone.gdml
 
+#Generate geometry with no cryostat 
+./generate_gdml.pl -w 0 -c 0 -i microboone-gdml-parameters.xml -o microboone-gdml-fragments.xml
+./make_gdml.pl -i microboone-gdml-fragments.xml -o microboone_LArTF.gdml
+
 #Copy both back up one level to be seen by LArSoft jobs
 cp microboone.gdml ..
 cp microboone_nowires.gdml ..
+cp microboone_LArTF.gdml ..
