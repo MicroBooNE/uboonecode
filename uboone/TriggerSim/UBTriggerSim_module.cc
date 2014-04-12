4a2f4418fafb159f8f916e502e75716fdbfc4c37
drinkingkazu <kazuhiro@nevis.columbia.edu>
2014-04-12 09:48:22 -0500

    MicroBooNE raw::Trigger producer module --kazu

---
+++ uboone/TriggerSim/UBTriggerSim_module.cc
@@ -0,0 +1,275 @@
+/**
+ * \file UBTriggerSim_module.cc
+ *
+ * \ingroup UBTriggerSim
+ * 
+ * \brief raw::Trigger producer module for microboone
+ *
+ * @author kazuhiro
+ */
+
+/** \addtogroup UBTriggerSim
+
+@{*/
+
+#ifndef UBTriggerSim_H
+#define UBTriggerSim_H
+
+// LArSoft includes
+//#include "Geometry/Geometry.h"
+
+// framework
+#include "fhiclcpp/ParameterSet.h"
+#include "messagefacility/MessageLogger/MessageLogger.h"
+#include "art/Framework/Core/EDProducer.h"
+#include "art/Framework/Principal/Event.h"
+#include "art/Framework/Principal/Handle.h"
+#include "art/Persistency/Common/Ptr.h"
+#include "art/Persistency/Common/PtrVector.h"
+#include "art/Framework/Services/Registry/ServiceHandle.h"
+#include "art/Framework/Services/Optional/TFileService.h"
+#include "art/Framework/Services/Optional/TFileDirectory.h"
+
+/// LArSoft
+#include "UBTrigException.h"
+#include "UBTriggerAlgo.h"
+#include "RawData/TriggerData.h"
+#include "OpticalDetectorData/PMTTrigger.h"
+#include "Utilities/AssociationUtil.h"
+#include "Utilities/ElecClock.h"
+#include "Utilities/TimeService.h"
+
+/// nutools
+#include "Simulation/BeamGateInfo.h"
+
+namespace trigger {
+  /**
+     \class UBTriggerSim
+     MicroBooNE's producer module for raw::Trigger data product.
+     Current implementation takes (1) trigger module configuration (UBTriggerAlgo configuration) and
+     (2) G4-event-wise Calibration, External, PC, BNB or NuMI trigger input with G4-time as time-stamp.
+     Note (2) is treated in the same way as pulse input into trigger module in real electronics.
+     It is possible to also set (2) per run or per sub-run, but such scheme is probably not useful for
+     the current implementation of event generation/simulation scheme. Talk to the author if such option
+     becomes useful/possible and needs to be implemented.
+  */ 
+  class UBTriggerSim : public art::EDProducer{
+  public:
+
+    /// Default ctor
+    UBTriggerSim(const fhicl::ParameterSet&);
+
+    /// Default dtor
+    ~UBTriggerSim();
+
+    /// art::EDProducer::produce implementation
+    virtual void produce (art::Event&); 
+
+  private:
+
+    /// Algorithm
+    UBTriggerAlgo fAlg;
+
+    /// Module labels for NuMI beam gate data product
+    std::vector<std::string> fBeamModNuMI;
+
+    /// Module labels for BNB beam gate data product
+    std::vector<std::string> fBeamModBNB;
+
+    /// Module label for OpticalFEM
+    std::string fOpticalFEMMod;
+
+    //-- ElecClock for user-defined trigger times --//
+    std::vector<util::ElecClock> fTriggerCalib; ///< user-defined calibration trigger (per-event)
+    std::vector<util::ElecClock> fTriggerPC;    ///< user-defined PC trigger (per-event)
+    std::vector<util::ElecClock> fTriggerExt;   ///< user-defined Ext trigger (per-event)
+    std::vector<util::ElecClock> fTriggerBNB;   ///< user-defined BNB trigger (per-event)
+    std::vector<util::ElecClock> fTriggerNuMI;  ///< user-defined NuMI trigger (per-event)
+
+  };
+
+} 
+
+#endif//  UBTriggerSim_H
+
+// UBTriggerSim.cc
+
+// Framework includes
+#include "art/Framework/Core/ModuleMacros.h"
+namespace trigger {
+  DEFINE_ART_MODULE(UBTriggerSim)
+}
+
+namespace trigger {
+
+  //#########################################################
+  UBTriggerSim::UBTriggerSim(fhicl::ParameterSet const& pset)
+  //#########################################################
+  {
+    /// Get NuMI event generator label
+    fBeamModNuMI = pset.get< std::vector<std::string> > ("BeamModNuMI", std::vector<std::string>());
+
+    /// Get BNB event generator label
+    fBeamModBNB  = pset.get< std::vector<std::string> > ("BeamModBNB",  std::vector<std::string>());
+
+    /// Get OpticalFEM module label
+    fOpticalFEMMod = pset.get< std::string > ("OpticalFEMMod","");
+
+    // Get trigger module configuration parameters
+    fAlg.SetDebugMode  ( pset.get< bool              > ("DebugMode"      ) );
+    fAlg.SetMask       ( pset.get< std::vector<bool> > ("Mask"           ) );
+    fAlg.SetPrescale   ( pset.get< std::vector<bool> > ("Prescale"       ) );
+    fAlg.SetDeadtime   ( pset.get< unsigned short    > ("DeadTime"       ) );
+    fAlg.SetBNBParams  ( pset.get< unsigned short    > ("BNBGateWidth"   ),
+			 pset.get< unsigned short    > ("BNBGateDelay"   ),
+			 pset.get< unsigned short    > ("BNBCosmicStart" ),
+			 pset.get< unsigned short    > ("BNBCosmicEnd"   ) );
+    fAlg.SetNuMIParams ( pset.get< unsigned short    > ("NuMIGateWidth"   ),
+			 pset.get< unsigned short    > ("NuMIGateDelay"   ),
+			 pset.get< unsigned short    > ("NuMICosmicStart" ),
+			 pset.get< unsigned short    > ("NuMICosmicEnd"   ) );
+
+    // Get user-defined trigger timings
+    std::vector<double> trig_calib ( pset.get< std::vector<double> > ("CalibTrigger",
+								      std::vector<double>()) 
+				     );
+    std::vector<double> trig_ext   ( pset.get< std::vector<double> > ("ExtTrigger",
+								      std::vector<double>()) 
+				     );
+    std::vector<double> trig_pc    ( pset.get< std::vector<double> > ("PCTrigger",
+								      std::vector<double>()) 
+				     );
+    std::vector<double> trig_bnb   ( pset.get< std::vector<double> > ("BNBTrigger",
+								      std::vector<double>()) 
+				     );
+    std::vector<double> trig_numi  ( pset.get< std::vector<double> > ("NuMITrigger",
+								      std::vector<double>()) 
+				     );
+
+    // Store user-defined trigger timings to the attributes
+    art::ServiceHandle<util::TimeService> ts;
+    auto clock = ts->OpticalClock();
+    fTriggerCalib.clear();
+    fTriggerExt.clear();
+    fTriggerPC.clear();
+    fTriggerBNB.clear();
+    fTriggerNuMI.clear();
+
+    fTriggerCalib.reserve(trig_calib.size());
+    for(auto const t : trig_calib) {
+      auto const elec_time = ts->G4ToElecTime(t);
+      clock.SetTime(clock.Sample(elec_time),clock.Frame(elec_time));
+      fTriggerCalib.push_back(clock);
+    }
+
+    fTriggerExt.reserve(trig_ext.size());
+    for(auto const t : trig_ext) {
+      auto const elec_time = ts->G4ToElecTime(t);
+      clock.SetTime(clock.Sample(elec_time),clock.Frame(elec_time));
+      fTriggerExt.push_back(clock);
+    }
+
+    fTriggerPC.reserve(trig_pc.size());
+    for(auto const t : trig_pc) {
+      auto const elec_time = ts->G4ToElecTime(t);
+      clock.SetTime(clock.Sample(elec_time),clock.Frame(elec_time));
+      fTriggerPC.push_back(clock);
+    }
+
+    fTriggerBNB.reserve(trig_bnb.size());
+    for(auto const t : trig_bnb) {
+      auto const elec_time = ts->G4ToElecTime(t);
+      clock.SetTime(clock.Sample(elec_time),clock.Frame(elec_time));
+      fTriggerBNB.push_back(clock);
+    }
+
+    fTriggerNuMI.reserve(trig_numi.size());
+    for(auto const t : trig_numi) {
+      auto const elec_time = ts->G4ToElecTime(t);
+      clock.SetTime(clock.Sample(elec_time),clock.Frame(elec_time));
+      fTriggerNuMI.push_back(clock);
+    }
+
+    // Produces raw::Trigger data product
+    produces< std::vector<raw::Trigger> >();
+  }
+
+  //###########################
+  UBTriggerSim::~UBTriggerSim()
+  //###########################
+  {}
+   
+  //#########################################
+  void UBTriggerSim::produce(art::Event& evt) 
+  //#########################################
+  {
+    // Initialize
+    art::ServiceHandle<util::TimeService> ts;
+    std::unique_ptr< std::vector<raw::Trigger>   >  triggers(new std::vector<raw::Trigger>);
+    fAlg.ClearInputTriggers();
+    auto clock = ts->OpticalClock();
+
+    // Register user-defined triggers
+    for( auto const &t : fTriggerCalib ) fAlg.AddTriggerCalib (t);
+    for( auto const &t : fTriggerExt   ) fAlg.AddTriggerExt   (t);
+    for( auto const &t : fTriggerPC    ) fAlg.AddTriggerPC    (t);
+    for( auto const &t : fTriggerBNB   ) fAlg.AddTriggerBNB   (t);
+    for( auto const &t : fTriggerNuMI  ) fAlg.AddTriggerNuMI  (t);
+
+    // Register input BNB beam trigger
+    for(auto const &name : fBeamModBNB) {
+      art::Handle<std::vector<sim::BeamGateInfo> > bnbArray;
+      evt.getByLabel(name,bnbArray);
+      
+      for(size_t i=0; i<bnbArray->size(); ++i) {
+	art::Ptr<sim::BeamGateInfo> beam_ptr (bnbArray,i);
+	auto const elec_time = ts->G4ToElecTime(beam_ptr->Start());
+	clock.SetTime(clock.Sample(elec_time),clock.Frame(elec_time));
+	fAlg.AddTriggerBNB(clock);
+      }
+    }
+
+    // Register input NuMI beam trigger
+    for(auto const &name : fBeamModNuMI) {
+      art::Handle<std::vector<sim::BeamGateInfo> > numiArray;
+      evt.getByLabel(name,numiArray);
+      
+      for(size_t i=0; i<numiArray->size(); ++i) {
+	art::Ptr<sim::BeamGateInfo> beam_ptr (numiArray,i);
+	auto const elec_time = ts->G4ToElecTime(beam_ptr->Start());
+	clock.SetTime(clock.Sample(elec_time),clock.Frame(elec_time));
+	fAlg.AddTriggerNuMI(clock);
+      }
+    }
+
+    // Register input PMTTrigger
+    if(!(fOpticalFEMMod.empty())) {
+
+      art::Handle<std::vector<optdata::PMTTrigger> > pmtArray;
+      evt.getByLabel(fOpticalFEMMod,pmtArray);
+      for(size_t i=0; i<pmtArray->size(); ++i) {
+	art::Ptr<optdata::PMTTrigger> pmt_ptr (pmtArray,i);
+	clock.SetTime( pmt_ptr->TimeSlice(), pmt_ptr->Frame() );
+	switch(pmt_ptr->Category()) {
+	case optdata::kCosmicPMTTrigger:
+	  fAlg.AddTriggerPMTCosmic(clock);
+	  break;
+	case optdata::kBeamPMTTrigger:
+	  fAlg.AddTriggerPMTBeam(clock);
+	  break;
+	default:
+	  throw UBTrigException(Form("Unexpected OpticalCategory (%d) found from PMTTrigger",pmt_ptr->Category()));
+	  return;
+	}
+      }
+    }
+
+    // Run trigger simulation
+    fAlg.ProcessTrigger(*triggers);
+
+    // Store
+    evt.put(std::move(triggers));
+  }
+} 
+/** @} */ // end of doxygen group 
+
