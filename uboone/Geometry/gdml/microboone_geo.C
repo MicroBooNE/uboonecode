2d0c833901c2e2bdd86820b178f7c4879ecd9e01
Ariana Hackenburg <ahack379@uboonegpvm04.fnal.gov>
2014-05-13 15:44:30 -0500

    no important changes

--- uboone/Geometry/gdml/microboone_geo.C
+++ uboone/Geometry/gdml/microboone_geo.C
@@ -9,7 +9,7 @@ microboone_geo(TString volName="")
   gSystem->Load("libGeom");
   gSystem->Load("libGdml");
 
-  TGeoManager::Import("microboone_LArTF.gdml");
+  TGeoManager::Import("microboone_nowires.gdml");
 
   drawopt optuboone[] = {
     {"volGround",       kOrange-7},
