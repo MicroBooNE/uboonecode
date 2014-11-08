afd922c43fb71780f99473a3137a4ab54e745b02
Eric Church <echurch@fnal.gov>
2014-11-08 11:53:52 -0600

    more path type-o fixes.

--- test/ci/uboone_hitcompare_new.sh
+++ test/ci/uboone_hitcompare_new.sh
@@ -4,4 +4,4 @@
 
 
 
-lar -c ${UBOONECODE_DIR}/job/hitfinder_ana_uboone.fcl -s ../lar_ci_hitana_reco2D/hitana_uboone_reco2D.root  -n -1 -T hitana_uboone_new_hist.root
+lar -c ${UBOONECODE_DIR}/job/hitfinder_ana_uboone.fcl -s ../lar_ci_hitana_reco2D_uboonecode/hitana_uboone_reco2D.root  -n -1 -T hitana_uboone_new_hist.root
