377cae6b27ff60bf34d762fd4e1840ec250338c0
Eric Church <echurch@fnal.gov>
2014-11-09 17:08:33 -0600

    Use the forward, rather than isotropic muons for the K-S test

--- test/ci/uboone_hitcompare_prod.sh
+++ test/ci/uboone_hitcompare_prod.sh
@@ -3,8 +3,9 @@
 # echurch@fnal.gov
 
 
-cp  ${UBOONECODE_DIR}/job/prod_muminus_0.1-2.0GeV_isotropic_uboone.fcl .
+#cp  ${UBOONECODE_DIR}/job/prod_muminus_0.1-2.0GeV_isotropic_uboone.fcl .
+cp  ${UBOONECODE_DIR}/job/prod_muminus_0.5-5.0GeV_25degf_uboone.fcl .
 #echo "services.user.FileCatalogMetadataExtras.RenameTemplate: '' " >> ./prod_muminus_0.1-2.0GeV_isotropic_uboone.fcl
 
-echo "outputs.out1.fileName: 'hitana_uboone_prod.root'" >> ./prod_muminus_0.1-2.0GeV_isotropic_uboone.fcl
-lar -c ./prod_muminus_0.1-2.0GeV_isotropic_uboone.fcl -n 60 -T hitana_uboone_prod_hist.root -o hitana_uboone_prod.root
+echo "outputs.out1.fileName: 'hitana_uboone_prod.root'" >> ./prod_muminus_0.5-5.0GeV_25degf_uboone.fcl
+lar -c ./prod_muminus_0.5-5.0GeV_25degf_uboone.fcl -n 60 -T hitana_uboone_prod_hist.root -o hitana_uboone_prod.root
