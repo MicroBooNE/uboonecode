755ff34889858e0cd53ba2e876c35bbdd933b4ac
Eric Church <echurch@fnal.gov>
2014-12-29 08:51:22 -0600

    Use ifdh cp to explicitly grab a few flux files, as /uboone/data seems not to be mounted to buildmaster slaves.

--- test/ci/prodgenietest.sh
+++ test/ci/prodgenietest.sh
@@ -4,7 +4,12 @@
 
 cp  ${UBOONECODE_DIR}/job/prodgenie_uboone.fcl .
 echo "services.user.FileCatalogMetadataExtras.RenameTemplate: '' " >> ./prodgenie_uboone.fcl
-export FW_SEARCH_PATH=${FW_SEARCH_PATH}:/uboone/data
+
+setup ifdhc
+# Just pick a few bnb flux files and put 'em here.
+ifdh cp /uboone/data/uboonebeam/bnb_gsimple_fluxes_02.28.2014_470/gsimple_microboone-470-onaxis_mc_nu_dummy_ntrd_4216_numintp_00002.root gsimple_microboone-470-onaxis_mc_nu_dummy_ntrd_4216_numintp_00002.root
+ifdh cp /uboone/data/uboonebeam/bnb_gsimple_fluxes_02.28.2014_470/gsimple_microboone-470-onaxis_mc_nu_dummy_ntrd_4216_numintp_00003.root gsimple_microboone-470-onaxis_mc_nu_dummy_ntrd_4216_numintp_00003.root
+ifdh cp /uboone/data/uboonebeam/bnb_gsimple_fluxes_02.28.2014_470/gsimple_microboone-470-onaxis_mc_nu_dummy_ntrd_4216_numintp_00004.root gsimple_microboone-470-onaxis_mc_nu_dummy_ntrd_4216_numintp_00004.root
 
 strace -o lar.strace lar -c ${UBOONECODE_DIR}/job/prodgenie_uboone.fcl -n 1 -o genie_gen.root -T genie_hist.root
 
