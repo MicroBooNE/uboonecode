8a7e455af6dae90ddf59d6e14199b8340b2f3144
Eric Church <echurch@fnal.gov>
2014-12-29 00:25:02 -0600

    Add /uboone/data explicitly to FW_SEARCH_PATH in order to find flux files. Not sure why this is necessary...

--- test/ci/prodgenietest.sh
+++ test/ci/prodgenietest.sh
@@ -4,5 +4,7 @@
 
 cp  ${UBOONECODE_DIR}/job/prodgenie_uboone.fcl .
 echo "services.user.FileCatalogMetadataExtras.RenameTemplate: '' " >> ./prodgenie_uboone.fcl
+export FW_SEARCH_PATH=${FW_SEARCH_PATH}:/uboone/data
+
 strace -o lar.strace lar -c ${UBOONECODE_DIR}/job/prodgenie_uboone.fcl -n 1 -o genie_gen.root -T genie_hist.root
 
