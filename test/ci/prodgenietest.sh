ff24b259dcc2262918e4008a64d35409cd0ed6de
Eric Church <echurch@fnal.gov>
2015-01-01 08:33:22 -0600

    Remove type-o

--- test/ci/prodgenietest.sh
+++ test/ci/prodgenietest.sh
@@ -4,6 +4,7 @@
 
 cp  ${UBOONECODE_DIR}/job/prodgenie_bnb_nu_uboone.fcl .
 echo "services.user.FileCatalogMetadataExtras.RenameTemplate: '' " >> ./prodgenie_bnb_nu_uboone.fcl
+
 #source `ups setup $SETUP_UPS`
 #source /grid/fermiapp/products/uboone/setup_uboone.sh;
 #setup ifdhc 
@@ -11,10 +12,8 @@ echo "services.user.FileCatalogMetadataExtras.RenameTemplate: '' " >> ./prodgeni
 #ifdh cp /uboone/data/uboonebeam/bnb_gsimple_fluxes_02.28.2014_470/gsimple_microboone-470-onaxis_mc_nu_dummy_ntrd_4216_numintp_00002.root gsimple_microboone-470-onaxis_mc_nu_dummy_ntrd_4216_numintp_00002.root
 #ifdh cp /uboone/data/uboonebeam/bnb_gsimple_fluxes_02.28.2014_470/gsimple_microboone-470-onaxis_mc_nu_dummy_ntrd_4216_numintp_00003.root gsimple_microboone-470-onaxis_mc_nu_dummy_ntrd_4216_numintp_00003.root
 #ifdh cp /uboone/data/uboonebeam/bnb_gsimple_fluxes_02.28.2014_470/gsimple_microboone-470-onaxis_mc_nu_dummy_ntrd_4216_numintp_00004.root gsimple_microboone-470-onaxis_mc_nu_dummy_ntrd_4216_numintp_00004.root
+
 export FW_SEARCH_PATH=${FW_SEARCH_PATH}:.
-
-echo "physics.producers.generator.FluxFiles: $1 " >> ./prodgenie_bnb_nu_uboone.fcl 
-gsimple_microboone-470-onaxis_mc_nu_dummy_ntrd_*.root
-
+echo "physics.producers.generator.FluxFiles: ' $1 ' " >> ./prodgenie_bnb_nu_uboone.fcl 
 strace -o lar.strace lar -c ${UBOONECODE_DIR}/job/prodgenie_bnb_nu_uboone.fcl -n 1 -o genie_gen.root -T genie_hist.root
 
