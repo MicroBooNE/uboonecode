44337b0b07ce4f238002859adcfc20cc2f264fd7
Xin Qian <xqian@uboonegpvm01.fnal.gov>
2015-01-06 15:52:46 -0600

    remove time offset in CalWire

--- uboone/CalData/CalWireMicroBooNE_module.cc
+++ uboone/CalData/CalWireMicroBooNE_module.cc
@@ -213,7 +213,8 @@ namespace caldata {
 
 	//David Caratelli
 	//subtract time-offset added in SImWireMicroBooNE_module
-	int time_offset = sss->FieldResponseTOffset(channel);
+	//Xin remove the time_offset
+	int time_offset = 0;//sss->FieldResponseTOffset(channel);
 	for(bin = 0; bin < dataSize; ++bin) {
 	  if ( (bin-time_offset >= 0) and (bin-time_offset < holder.size())  )
 	  holder[bin-time_offset]=(rawadc[bin]-pdstl);
