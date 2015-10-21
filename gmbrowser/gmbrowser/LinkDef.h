92b1e7a19c4c033e76a23f7994dd834dcd14e134
Mike Kirby <kirby@fnal.gov>
2015-10-21 14:35:11 -0500

    Removing the gmbrowser directory since it is a CVS repo.

--- gmbrowser/gmbrowser/LinkDef.h
+++
@@ -1,8 +0,0 @@
-#ifdef __CINT__
-#pragma link off all classes;
-#pragma link off all functions;
-#pragma link off all globals;
-
-#pragma link C++ class GMBrowser;
-//#pragma link C++ function GMBrowser::catch_event;
-#endif
