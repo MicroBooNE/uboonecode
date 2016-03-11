d1a635bf5504459d8fd84b77e88798a6424d4560
Tingjun Yang <tjyang@fnal.gov>
2016-03-10 21:37:45 -0600

    change the minimal track length requirement to 10 cm for MCS calculation.

--- uboone/AnalysisTree/AnalysisTree_module.cc
+++ uboone/AnalysisTree/AnalysisTree_module.cc
@@ -3670,7 +3670,8 @@ void microboone::AnalysisTree::analyze(const art::Event& evt)
     
       //call the track momentum algorithm that gives you momentum based on track range
       trkf::TrackMomentumCalculator trkm;
-       
+      trkm.SetMinLength(10); //change the minimal track length requirement to 10 cm
+
       for(size_t iTrk=0; iTrk < NTracks; ++iTrk){//loop over tracks
       
         //save t0 from reconstructed flash track matching for every track
