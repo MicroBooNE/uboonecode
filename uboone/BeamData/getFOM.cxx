424efab2546139d4a604601b7801d906be2bd0a5
Zarko Pavlovic <zarko@uboonegpvm01.fnal.gov>
2016-05-12 00:24:16 -0500

    Merge branch 'develop' of ssh://cdcvs.fnal.gov/cvs/projects/uboonecode into develop

---
+++ uboone/BeamData/getFOM.cxx
@@ -0,0 +1,12 @@
+#include "getFOM.h"
+#include <iostream>
+
+using namespace gov::fnal::uboone::datatypes;
+
+float bmd::getFOM(std::string beam, const ub_BeamHeader& bh, const std::vector<ub_BeamData>& bd)
+{
+  //placeholder for FOM calculation
+  float fom=1;
+
+  return fom;  
+}
